dacfffaaa100a5786ba8bb1d943e2cca6cee6c9f
migraf <michael.graf3110@gmail.com>
2017-03-05 21:24:19 +0000

    added timestamp functionality with moment.js

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
-var express = require("express")
-var express = require('express')
-var app = express()
+var express = require("express");
+var moment = require("moment");
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 
-app.get('/', function(request, response) {
-  response.send("Hello World!")
+app.get('/:datestring', function(request, response) {
+    var dateString = request.params.datestring;
+    var date;
+    if(/^\d{8,}$/.test(dateString)){
+        date = moment(dateString, "X");
+    }
+    else {
+        date = moment(dateString, "MMMM D, YYYY")
+    }
+    
+    if(date.isValid()){
+        response.json({"unix": date.format("X"), "natural": date.format("MMMM D, YYYY")});
+    }
+    else{
+        response.json({"unix": null, "natural": null});
+    }
+    
+    
 });
 
 app.listen(app.get('port'), function() {
